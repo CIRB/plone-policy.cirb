b3bebec4ce451d196da5898295d831d26999e514
Benoît Suttor <bsuttor@cirb.irisnet.be>
2012-12-20 16:51:21 +0100

    add monitoring

--- setup.py
+++ setup.py
@@ -4,60 +4,59 @@ import os
 version = '1.2.dev0'
 
 long_description = (
-    open('README.txt').read()
-    + '\n' +
-    'Contributors\n'
-    '============\n'
-    + '\n' +
-    open('CONTRIBUTORS.txt').read()
-    + '\n' +
-    open('CHANGES.txt').read()
-    + '\n')
+        open('README.txt').read()
+        + '\n' +
+        'Contributors\n'
+        '============\n'
+        + '\n' +
+        open('CONTRIBUTORS.txt').read()
+        + '\n' +
+        open('CHANGES.txt').read()
+        + '\n')
 
 setup(name='policy.cirb',
-      version=version,
-      description="'policy of CIRB-CIBG site'",
-      long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-        "Programming Language :: Python",
-        ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
-      license='gpl',
-      packages=find_packages('src'),
-      package_dir = {'': 'src'},
-      namespace_packages=['policy', ],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-          'plonetheme.cirb',
-	  'Products.PloneFormGen',
-	  'collective.ckeditor',
-	  'collective.plonefinder',
-	  'collective.quickupload',
-	  'quintagroup.analytics',
-	  'fourdigits.portlet.twitter',
-	  'collective.anysurfer',
-          'Products.PloneHelpCenter',
-	  'collective.contentstats',
-	  'collective.recaptcha',
-          'Products.CirbCountdown',
-	  'Products.Collage',
- 	  'webcouturier.dropdownmenu',
-          'collective.collage.portlets'
-      ],
-      extras_require={'test': ['plone.app.testing']},
-      entry_points="""
+        version=version,
+        description="'policy of CIRB-CIBG site'",
+        long_description=long_description,
+        # Get more strings from
+        # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        classifiers=[
+            "Programming Language :: Python",
+            ],
+        keywords='',
+        author='',
+        author_email='',
+        url='http://svn.plone.org/svn/collective/',
+        license='gpl',
+        packages=find_packages('src'),
+        package_dir = {'': 'src'},
+        namespace_packages=['policy', ],
+        include_package_data=True,
+        zip_safe=False,
+        install_requires=[
+            'setuptools',
+            # -*- Extra requirements: -*-
+            'plonetheme.cirb',
+            'Products.PloneFormGen',
+            'collective.ckeditor',
+            'collective.plonefinder',
+            'collective.quickupload',
+            'quintagroup.analytics',
+            'fourdigits.portlet.twitter',
+            'collective.anysurfer',
+            'Products.PloneHelpCenter',
+            'collective.contentstats',
+            'collective.recaptcha',
+            'Products.CirbCountdown',
+            'Products.Collage',
+            'webcouturier.dropdownmenu',
+            'collective.collage.portlets',
+            'cirb.zopemonitoring',
+            ],
+        extras_require={'test': ['plone.app.testing']},
+        entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["templer.localcommands"],
       )
